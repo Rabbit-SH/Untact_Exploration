bac30e290ae22d3efbefdfd395268cd55a91ed6e
Jinujara <128118523+Jinujara@users.noreply.github.com>
2023-11-30 17:06:48 +0900

    코스 선택 수정2

--- front-end/src/views/ChooseCourse.vue
+++ front-end/src/views/ChooseCourse.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <v-img :src="require('@/assets/tutorialView/코스 선택.png')" class="course_img"></v-img>
-    <div class="A"></div>
-    <div class="B" @click="$router.push({name: 'TutorialPage'})"></div>
+    <v-img :src="require('@/assets/tutorialView/코스 선택.png')" class="course_img">
+      <div class="A"></div>
+      <v-btn class="B" @click="$router.push({name: 'TutorialPage'})"></v-btn>
+    </v-img>
+
+    
   </div>
 </template>
 
@@ -18,21 +21,28 @@ export default {
 .A,
 .B {
   position: absolute;
-
-  width: 30%; /* 이미지에 따라 조절하세요 */
-  height: 30%;
-  /* background-color: white; */
-  background-color: transparent; /*배경을 투명하게 만듭니다*/
-  cursor: pointer; /* 마우스를 올렸을 때 포인터로 변경됩니다 */
-}
-.A {
-  top: 18%;
-  left: 30%;
-}
-.B {
   bottom: 7%;
   left: 57%;
   width: 36%;
   height: 19%;
+
+}
+@media (max-width: 600px) {
+  .B {
+    bottom: 7%;
+    left: 57%;
+    width: 36%;
+    height: 19%;
+  }
+}
+.theme--light.v-btn.v-btn--has-bg {
+    background-color: transparent;
+}
+.v-btn:not(.v-btn--round).v-size--default {
+    height: 19%;
+    min-width: 64px;
+}
+.v-btn--is-elevated {
+    box-shadow: none;
 }
 </style>
